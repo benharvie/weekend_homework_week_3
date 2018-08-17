262b2e7ae289cf60adb169380396849b25ea4293
Ben Harvie <benharvie94@gmail.com>
2018-08-17 14:42:02 +0100

    all save methods complete

--- console.rb
+++ console.rb
@@ -12,5 +12,24 @@ customer2.save
 customer3 = Customer.new({"name" => "Crash Bandicoot", "funds" => "30"})
 customer3.save
 
+film1 = Film.new({"title" => "Step Brothers", "price" => "10"})
+film1.save
+
+film2 = Film.new({"title" => "Enter The Matrix", "price" => "7"})
+film2.save
+
+film3 = Film.new({"title" => "The 300", "price" => "4"})
+film3.save
+
+ticket1 = Ticket.new({"customer_id" => customer1.id, "film_id" => film1.id})
+ticket1.save
+
+ticket2 = Ticket.new({"customer_id" => customer2.id, "film_id" => film2.id})
+ticket2.save
+
+ticket3 = Ticket.new({"customer_id" => customer3.id, "film_id" => film3.id})
+ticket3.save
+
+
 binding.pry
 nil
