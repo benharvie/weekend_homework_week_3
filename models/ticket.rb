73e6a065d8e6235e09f8937df537dda892dbef24
Ben Harvie <benharvie94@gmail.com>
2018-08-17 14:59:09 +0100

    all update functions working

--- models/ticket.rb
+++ models/ticket.rb
@@ -1,7 +1,8 @@
 require_relative '../db/sql_runner'
 
 class Ticket
-  attr_reader :id, :customer_id, :film_id
+  attr_reader :id
+  attr_accessor :customer_id, :film_id
   def initialize(options)
     @id = options['id'].to_i if options['id']
     @customer_id = options['customer_id'].to_i
@@ -16,6 +17,16 @@ class Ticket
     RETURNING id"
     values = [@customer_id, @film_id]
     @id = SqlRunner.run(sql, values)[0]['id'].to_i
+  end
+
+  def update
+    sql = "UPDATE tickets
+    SET (customer_id, film_id)
+    =
+    ($1, $2)
+    WHERE id = $3"
+    values = [@customer_id, @film_id, @id]
+    SqlRunner.run(sql, values)
   end
 
   def self.delete_all
