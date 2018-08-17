472c49233ad0b81ad6436a5cb06fc39d95db56e1
Ben Harvie <benharvie94@gmail.com>
2018-08-17 14:52:58 +0100

    customer update working

--- models/customer.rb
+++ models/customer.rb
@@ -1,8 +1,8 @@
 require_relative '../db/sql_runner'
 
 class Customer
-  attr_accessor :funds
-  attr_reader :id, :name
+  attr_accessor :name, :funds
+  attr_reader :id
   def initialize(options)
     @id = options['id'].to_i if options['id']
     @name = options['name']
@@ -17,6 +17,16 @@ class Customer
     RETURNING id"
     values = [@name, @funds]
     @id = SqlRunner.run(sql, values)[0]['id'].to_i
+  end
+
+  def update
+    sql = "UPDATE customers
+    SET (name, funds)
+    =
+    ($1, $2)
+    WHERE id = $3"
+    values = [@name, @funds, @id]
+    SqlRunner.run(sql, values)
   end
 
   def self.delete_all
