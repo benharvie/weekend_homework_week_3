084ad98ba776102021756077a08713dcc12f9a70
Ben Harvie <benharvie94@gmail.com>
2018-08-18 23:37:32 +0100

    Test customer has funds

---
+++ models/specs/customer_spec.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require 'minitest/rg'
+require_relative '../customer.rb'
+
+class CustomerTest < MiniTest::Test
+  def setup
+    @customer1 = Customer.new({ "name" => "Ben Harvie", "funds" => "90" })
+    @customer2 = Customer.new({ "name" => "Neiko Fraser", "funds" => "50" })
+    @customer3 = Customer.new({ "name" => "Crash Bandicoot", "funds" => "30" })
+  end
+
+  def test_customer_has_funds
+    assert_equal(90, @customer1.funds)
+  end
+end
