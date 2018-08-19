1be357792b23c6fd5f346d1984ceb095979ee0f3
Ben Harvie <benharvie94@gmail.com>
2018-08-19 12:34:50 +0100

    screenings table added to sql, sql refactored

--- db/cinema.sql
+++ db/cinema.sql
@@ -4,14 +4,21 @@ DROP TABLE films;
 
 CREATE TABLE customers (
   id SERIAL8 PRIMARY KEY,
-  name VARCHAR(255),
-  funds INT8
+  name VARCHAR(255) NOT NULL,
+  funds INT8 NOT NULL
 );
 
 CREATE TABLE films (
   id SERIAL8 PRIMARY KEY,
-  title VARCHAR(255),
-  price INT8
+  title VARCHAR(255) NOT NULL,
+  price INT8 NOT NULL
+);
+
+CREATE TABLE screenings(
+	id SERIAL8 PRIMARY KEY,
+	show_time TIME NOT NULL,
+	film_id INT8 REFERENCES films(id),
+	capacity INT8 NOT NULL
 );
 
 CREATE TABLE tickets (
